d2a105af2e2f5fecff3e21aa498c1f125d604849
Fazilmd152 <mohamedfazil152@gmail.com>
2025-01-16 00:10:02 +0530

    Build completed

--- server/server.js
+++ server/server.js
@@ -7,7 +7,9 @@ import reviewRoutes from './routes/reviewRoute.js'
 import error from './middlewares/error.js'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
+import path from 'path'
 
+const __dirname = path.resolve()
 const app = express()
 dotenv.config()
 
@@ -25,6 +27,14 @@ app.use("/api/review/", reviewRoutes) //review routes
 
 
 app.use(error) //this middleware caught and send custom errors
+
+
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,'/client/build')))
+    app.use('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,"client","build","index.html"))
+    })
+}
 
 app.listen(process.env.PORT, () => {
     console.log(`Server started on port ${process.env.PORT} `);
